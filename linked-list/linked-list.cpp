cd81b80a319b6965e8a2a24ff31c98fb4961e928
DiassIgor <diassigor@hotmail.com>
2023-03-24 15:47:45 +0200

    feat: Added insert function implementation

--- linked-list/linked-list.cpp
+++ linked-list/linked-list.cpp
@@ -1,1 +1,21 @@
 #include <linked-list.h>
+#include <iostream>
+#include <cassert>
+
+void LinkedList::insert(string content){
+
+    node *new_node = (node *) malloc(sizeof(node));
+    (*new_node).content = content;
+    (*new_node).next = NULL;
+    assert(new_node=NULL);
+
+    if (_init_pointer=NULL){
+        _init_pointer = new_node;
+        _final_pointer = new_node;
+    }
+    else{
+        (*_final_pointer).next = new_node;
+        _final_pointer = new_node;
+    }
+
+};
