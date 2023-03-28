c73861ca6ea1d9b13cb090a4befc63ffc44c8253
DiassIgor <diassigor@hotmail.com>
2023-03-28 12:04:12 +0300

    test: Added more unit tests for pos_insert function

--- linked-list/Main.cpp
+++ linked-list/Main.cpp
@@ -50,14 +50,36 @@ void test_pos_insert2(){
     link_list2.pos_insert("Item 8", 0);
     string actual = link_list2.test_list();
     string expected = "Item 8 | Item 1 | Item 2 | Item 3 | Item 4 | Item 5 | Item 6 | Item 7 | ";
+    test_layout(actual, expected, "Pos_insert2");
+};
+
+void test_pos_insert3(){
+
+    LinkedList link_list3 = standard_list();
+    link_list3.pos_insert("Item 8", 4);
+    string actual = link_list3.test_list();
+    string expected = "Item 1 | Item 2 | Item 3 | Item 4 | Item 8 | Item 5 | Item 6 |  Item 7 | ";
+    test_layout(actual, expected, "Pos_insert3");
+};
+
+void test_pos_insert4(){
+
+    LinkedList link_list4;
+    link_list4.pos_insert("Item 1", 0);
+    link_list4.pos_insert("Item 2", 1);
+    string actual = link_list4.test_list();
+    string expected = "Item 1 | Item 2 | ";
+    test_layout(actual, expected, "Pos_insert4");
 };
 
 int main(){
 
-    test_pos_insert0();
-    cout << "\n test \n";
-    test_pos_insert1();
+    //test_pos_insert0();
+    //cout << "\n test \n";
+    //test_pos_insert1();
     //test_pos_insert2();
+    //test_pos_insert3();
+    test_pos_insert4();
 
     return 0;
 };
