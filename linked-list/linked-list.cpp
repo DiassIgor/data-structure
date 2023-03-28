c6781e204eb6e138a798f0e6e79b20e5a7e0c39a
DiassIgor <diassigor@hotmail.com>
2023-03-28 14:01:19 +0300

    refactor: Added arrow operator sintaxe

--- linked-list/linked-list.cpp
+++ linked-list/linked-list.cpp
@@ -5,50 +5,47 @@
 void LinkedList::insert(string content){
 
     node *new_node = (node *) malloc(sizeof(node));
-    (*new_node).content = content;
-    (*new_node).next = NULL;
+    new_node->content = content;
+    new_node->next = NULL;
     assert(new_node != NULL);
 
-    if (_init_pointer==NULL){
+    if (_init_pointer == NULL){
         _init_pointer = new_node;
         _final_pointer = new_node;
     }
     else{
-        (*_final_pointer).next = new_node;
+        _final_pointer->next = new_node;
         _final_pointer = new_node;
     }
 
     _list_size += 1;
-
 };
 
 void LinkedList::pos_insert(string content, size_t position){
 
     if (position > _list_size || position < 0){
-    
-        cout << "Wrong position value!\n";
 
+        cout << "Wrong position value!\n";
     }
 
     else{
 
         node *new_node = (node *) malloc(sizeof(node));
         assert(new_node != NULL);
-        
-        (*new_node).content = content;
-        (*new_node).next = NULL;
+
+        new_node->content = content;
+        new_node->next = NULL;
 
         if(position == 0){
 
-            (*new_node).next = _init_pointer;
+            new_node->next = _init_pointer;
             _init_pointer = new_node;
         } 
 
         else if(position == _list_size){
 
-            (*_final_pointer).next = new_node;
+            _final_pointer->next = new_node;
             _final_pointer = new_node;
-
         }
 
         else{
@@ -56,42 +53,40 @@ void LinkedList::pos_insert(string content, size_t position){
             node *aux_pointer = _init_pointer;
             
             for(int i=0;i<position-1;i++){
-                aux_pointer = (*aux_pointer).next;
-                cout << (*aux_pointer).content;
+                aux_pointer = aux_pointer->next;
             }
 
-            (*new_node).next = (*aux_pointer).next;
-            (*aux_pointer).next = new_node;
+            new_node->next = aux_pointer->next;
+            aux_pointer->next = new_node;
         }
 
     _list_size += 1;
-
     }
 }
 
 void LinkedList::show_list(){
     
-    node *aux_point = _init_pointer; 
+    node *aux_pointer = _init_pointer; 
     cout << "Direction: First -> Last\n";
 
     for(int i=0; i<_list_size; i++){
-        cout << (*aux_point).content << " | ";
-        aux_point = (*aux_point).next;
+        cout << aux_pointer->content << " | ";
+        aux_pointer = aux_pointer->next;
     }
 
     cout << "\n-----------------------------------------------------------------\n";
-
 };
 
 string LinkedList::test_list(){
 
-    node *aux_point = _init_pointer;
+    node *aux_pointer = _init_pointer;
     string aux_string = "";
 
     for(int i=0; i<_list_size; i++){
-        aux_string.append((*aux_point).content);
+        
+        aux_string.append(aux_pointer->content);
         aux_string.append("|");
-        aux_point = (*aux_point).next;
+        aux_pointer = aux_pointer->next;
     }
 
     return aux_string;
