a68f37ff09453ff5ab1d1f3aa7f3a15c56960284
DiassIgor <diassigor@hotmail.com>
2023-04-04 17:03:02 +0300

    test: Implementation of more tests for remove function

--- linked-list/Main.cpp
+++ linked-list/Main.cpp
@@ -28,8 +28,8 @@ void test_layout(string actual, string expected, string test_name){
 // Test if standard list works properly
 void test_pos_insert0(){
 
-    LinkedList link_list0 = standard_list();
-    string actual = link_list0.test_list();
+    LinkedList link_list = standard_list();
+    string actual = link_list.test_list();
     string expected = "Item 1|Item 2|Item 3|Item 4|";
     test_layout(actual, expected, "Pos_insert0");
 };
@@ -37,9 +37,9 @@ void test_pos_insert0(){
 // Test if pos_insertion in a empty linked list works properly
 void test_pos_insert1(){
 
-    LinkedList link_list1;
-    link_list1.pos_insert("Item *", 0);
-    string actual = link_list1.test_list();
+    LinkedList link_list;
+    link_list.pos_insert("Item *", 0);
+    string actual = link_list.test_list();
     string expected = "Item *|";
     test_layout(actual, expected, "Pos_insert1");
 };
@@ -47,9 +47,9 @@ void test_pos_insert1(){
 // Test if pos_insertion in initial position works properly
 void test_pos_insert2(){
 
-    LinkedList link_list2 = standard_list();
-    link_list2.pos_insert("Item *", 0);
-    string actual = link_list2.test_list();
+    LinkedList link_list = standard_list();
+    link_list.pos_insert("Item *", 0);
+    string actual = link_list.test_list();
     string expected = "Item *|Item 1|Item 2|Item 3|Item 4|";
     test_layout(actual, expected, "Pos_insert2");
 };
@@ -57,9 +57,9 @@ void test_pos_insert2(){
 // Test if pos_insertion in position 1 works properly
 void test_pos_insert3(){
 
-    LinkedList link_list3 = standard_list();
-    link_list3.pos_insert("Item *", 1);
-    string actual = link_list3.test_list();
+    LinkedList link_list = standard_list();
+    link_list.pos_insert("Item *", 1);
+    string actual = link_list.test_list();
     string expected = "Item 1|Item *|Item 2|Item 3|Item 4|";
     test_layout(actual, expected, "Pos_insert3");
 };
@@ -67,9 +67,9 @@ void test_pos_insert3(){
 // Test if pos_insertion in last position works properly
 void test_pos_insert4(){
 
-    LinkedList link_list4 = standard_list();
-    link_list4.pos_insert("Item *", 4);
-    string actual = link_list4.test_list();
+    LinkedList link_list = standard_list();
+    link_list.pos_insert("Item *", 4);
+    string actual = link_list.test_list();
     string expected = "Item 1|Item 2|Item 3|Item 4|Item *|";
     test_layout(actual, expected, "Pos_insert4");
 };
@@ -77,9 +77,9 @@ void test_pos_insert4(){
 // Test if pos_insertion in next-to-last position works properly
 void test_pos_insert5(){
 
-    LinkedList link_list5 = standard_list();
-    link_list5.pos_insert("Item *", 3);
-    string actual = link_list5.test_list();
+    LinkedList link_list = standard_list();
+    link_list.pos_insert("Item *", 3);
+    string actual = link_list.test_list();
     string expected = "Item 1|Item 2|Item 3|Item *|Item 4|";
     test_layout(actual, expected, "Pos_insert5");
 };
@@ -104,6 +104,36 @@ void test_remove1(){
 
 };
 
+// Test if the delete of the node in last position works properly
+void test_remove2(){
+    LinkedList link_list = standard_list();
+    link_list.remove(3);
+    string actual = link_list.test_list();
+    string expected = "Item 1|Item 2|Item 3|";
+    test_layout(actual, expected, "Delete2");
+
+};
+
+// Test if the delete of the node in next-to-last position works properly
+void test_remove3(){
+    LinkedList link_list = standard_list();
+    link_list.remove(2);
+    string actual = link_list.test_list();
+    string expected = "Item 1|Item 2|Item 4|";
+    test_layout(actual, expected, "Delete3");
+
+};
+
+// Test if the delete of the node in position 1 works properly in a list with 1 node
+void test_remove4(){
+    LinkedList link_list;
+    link_list.insert("Item 1");
+    link_list.remove(0);
+    string actual = link_list.test_list();
+    string expected = "";
+    test_layout(actual, expected, "Delete4");
+
+};
 
 int main(){
 
@@ -114,7 +144,10 @@ int main(){
     test_pos_insert4(); //OK
     test_pos_insert5(); //OK
     test_remove0(); //OK
-    test_remove1();
-
+    test_remove1(); //OK
+    test_remove2(); //OK
+    test_remove3(); //Error
+    test_remove4(); //OK
+    
     return 0;
 };
